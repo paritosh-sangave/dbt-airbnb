ce14f78ac18c368449663ea3628e360accc9f950
paritosh-sangave <pari.sangave@gmail.com>
2026-01-18 17:57:10 +0530

    Add reviews fct reviews and dim listings w hosts

---
+++ airbnb/models/dim/dim_listings_w_hosts.sql
@@ -0,0 +1,27 @@
+WITH
+    listings AS (
+        SELECT
+            *
+        FROM
+            {{ ref('dim_listings_cleansed') }}
+    ),
+    hosts AS (
+        SELECT
+            *
+        FROM
+            {{ ref('dim_hosts_cleansed') }}
+    )
+SELECT
+    listings.listing_id,
+    listings.listing_name,
+    listings.room_type,
+    listings.minimum_nights,
+    listings.price,
+    listings.host_id,
+    hosts.host_name,
+    hosts.is_superhost as host_is_superhost,
+    listings.created_at,
+    GREATEST (listings.updated_at, hosts.updated_at) as updated_at
+FROM
+    listings
+    LEFT JOIN hosts ON (hosts.host_id = listings.host_id)
