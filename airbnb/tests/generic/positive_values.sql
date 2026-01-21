2f04f3ea39c7d6e9c2efdf26bc6ddab9e7ac54b1
paritosh-sangave <pari.sangave@gmail.com>
2026-01-21 12:00:25 +0530

    Format SQL in test

--- airbnb/tests/generic/positive_values.sql
+++ airbnb/tests/generic/positive_values.sql
@@ -1,3 +1,7 @@
 {% test positive_values(model, column_name) %}
-SELECT * FROM {{ model }} WHERE {{ column_name }} <= 0
+SELECT 
+    * 
+FROM 
+    {{ model }} 
+    WHERE {{ column_name }} <= 0
 {% endtest %}
